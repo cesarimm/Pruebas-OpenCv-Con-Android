5633ad8d89af8f7f55723532478d361b4a61b8a8
CesarIvanMartinez <bmwcesarim@gmail.com>
2020-01-29 12:52:56 -0600

    Pantalla de Inicio 50%

---
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void openCamera(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
+
+/*
+    <item
+        android:id="@+id/ventanaUno"
+        android:orderInCategory="100"
+        android:tittle="@string/ventana1"
+        app:showAsAction="never"
+        />
+
+    <item
+        android:id="@+id/ventanaDos"
+        android:orderInCategory="100"
+        android:tittle="@string/ventana2"
+        app:showAsAction="never"
+        />
+
+    <item
+        android:id="@+id/ventanaTres"
+        android:orderInCategory="100"
+        android:tittle="@string/ventana3"
+        app:showAsAction="never"
+        />
+ */
